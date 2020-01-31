2864c2bc87a59ccfb889fca62bedade84d839b87
GitCrey <assassincreydt@gmail.com>
2020-01-31 09:23:46 +0100

    yoink

---
+++ JS/Main.js
@@ -0,0 +1,10 @@
+const part1Answer = document.querySelectorAll("input[name='answer1']");
+const part2Answer = document.querySelectorAll("input[name='answer2']");
+const part3Answer = document.querySelectorAll("input[name='answer3']");
+const part4Answer = document.querySelectorAll("input[name='answer4']");
+const part5Answer = document.querySelectorAll("input[name='answer5']");
+const part6Answer = document.querySelectorAll("input[name='answer6']");
+const part7Answer = document.querySelectorAll("input[name='answer7']");
+const part8Answer = document.querySelectorAll("input[name='answer8']");
+
+const parts = document.querySelectorAll(".question");
