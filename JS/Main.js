6f9d9f62c161341d6c62960d370c48f190df0fa7
GitCrey <assassincreydt@gmail.com>
2020-02-07 09:40:55 +0100

    All

--- JS/Main.js
+++ JS/Main.js
@@ -1,3 +1,5 @@
+let pictures []
+
 const part1Answer = document.querySelectorAll("input[name='answer1']");
 const part2Answer = document.querySelectorAll("input[name='answer2']");
 const part3Answer = document.querySelectorAll("input[name='answer3']");
@@ -6,6 +8,30 @@ const part5Answer = document.querySelectorAll("input[name='answer5']");
 const part6Answer = document.querySelectorAll("input[name='answer6']");
 const part7Answer = document.querySelectorAll("input[name='answer7']");
 const part8Answer = document.querySelectorAll("input[name='answer8']");
+
+/*const picture1Answer = document.querySelectorAll("src[name='answer1']");
+picture1Answer.addEventListener("click", addPicture);*/
+
+
+/*
+addPicture("img/cpu.png", "Bild på CPU", document.querySelector("#content-root"));
+addPicture("img/gpu.png", "Bild på GPU", document.querySelector("#content-root"));
+
+function addPicture(source, alt, location)
+{
+    let newPicture = document.createElement("img");
+    newPicture.setAttribute("src", source);
+    newPicture.setAttribute("alt", alt);
+    location.appendChild(newPicture);
+}
+
+changePicture("my-picture", "img/newPicture.png", "a new nice picture");
+
+function changePicture(id, src, alt) {
+    document.getElementById(id).setAttribute("src", src);
+    document.getElementById(id).setAttribute("alt", alt);
+} */
+
 
 const questions = document.querySelectorAll(".question");
 let currentQuestion = 0;
